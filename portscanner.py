cb2404776b676a8c59d5f3cdfd0a87ea33f58999
Chris <slybandit247@gmail.com>
2022-05-15 16:11:42 -0400

    Update portscanner.py
    Add Handlers for using ctrl+c to exit the program cleanly at any point.

--- portscanner.py
+++ portscanner.py
@@ -22,20 +22,29 @@ def port_scan(ipaddress, port):
         sock.connect((ipaddress, port))
         print('[+] Port **%d** OPEN' % port)
     except KeyboardInterrupt:
-        print("[!]Program exited by user.")
+        print("\r\n[!]Program exited by user.")
         sys.exit(1)
     except:
         print('[-] Port %d CLOSED' % port)
-
-IPADDRESS = input("$ Enter Target to Scan: ")
-port = (input("$ Enter the last port number you want to scan: \r\n$Or Enter 'C' if you want to scan the most common ports (1-1024): "))
-if port == "C" or "c":
-    port = range(1024)
-else:
-    port = range(port)
+try:
+    IPADDRESS = input("$ Enter Target to Scan: ")
+except KeyboardInterrupt:
+    print("\r\n[!]Program exited by user.")
+    sys.exit(1)
+try:
+    port = (input("$ Enter the last port number you want to scan \r\n$ Enter 'C' if you want to scan the most common ports (1-1024): "))
+    if port == "C" or "c":
+        port = range(1024)
+    else:
+        port = range(port)
+except KeyboardInterrupt:
+    print("\r\n[!]Program exited by user.")
+    sys.exit(1)
 print("[*]Host:",socket.gethostbyname(IPADDRESS), "scan started at", START)
 converted_ip = checkIP(IPADDRESS)
+
 def do_scan():
         for i in port:
             port_scan(converted_ip, port[i])
+
 do_scan()
